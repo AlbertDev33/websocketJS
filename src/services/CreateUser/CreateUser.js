5acf8ed63a9d2f1da8cb658b23d10c3868b1ca19
Albert <albert.rocha@r2t.company>
2021-09-20 20:52:49 -0300

    feature: email validation

--- src/services/CreateUser/CreateUser.js
+++ src/services/CreateUser/CreateUser.js
@@ -1,12 +1,24 @@
+import { CreateUserError } from '../../shared/errors/CreateUserError';
+
+const USER_ERROR_MESSAGE = 'User already exists.';
+
 export class CreateUser {
-    constructor(validationBody, userRepository) {
+    constructor(validationBody, userRepository, hashPassword) {
         this.validationBody = validationBody;
         this.userRepository = userRepository;
+        this.hashPassword = hashPassword;
     }
 
     async execute(body) {
         this.validationBody.validation(body);
 
+        const userAlreadyExists = await this.userRepository.findByEmail(body);
+
+        if (userAlreadyExists) {
+            throw new CreateUserError(USER_ERROR_MESSAGE);
+        }
+
+        body.password = await this.hashPassword.generateHash(body.password);
         const user = await this.userRepository.createUser(body);
 
         return user;
