f122a387ec2f643d229c495b559ec937b1dd1c60
Albert <albert.rocha@r2t.company>
2021-09-20 20:00:53 -0300

    feature: created comment

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -13,6 +13,8 @@ export class UserController {
 
         const token = this.jwt.createJWT(user.userId);
 
+        // create a cookie name as jwt and contain token and expire after 1 day
+	    // in cookies, expiration date calculate by milisecond
         const maxAge = this.#MAX_AGE * 1000;
         response.cookie('jwt', token, { httpOnly: true,  maxAge});
         return response.status(201).json(user)
