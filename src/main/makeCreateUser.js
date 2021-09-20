13942d8a0fc980dc3aee92f2a8ab0a4ee44d7c3b
Albert <albert.rocha@r2t.company>
2021-09-20 20:53:10 -0300

    feature: created new instance

--- src/main/makeCreateUser.js
+++ src/main/makeCreateUser.js
@@ -4,12 +4,14 @@ import { UserRepository } from "../shared/infra/repository/UserRepository"
 import { MongoConnection } from '../shared/infra/database/mongoConnection';
 import { CreateUser } from "../services/CreateUser/CreateUser";
 import { ValidationRequestBody } from "../services/ValidationRequestBody/ValidationRequestBody";
+import { HashProvider } from '../shared/providers/HashPassword/HashPassword';
 
 export const makeCreateUser = () => {
     const mongoConnection = new MongoConnection();
     const userRepository = new UserRepository(mongoConnection);
     const validationBody = new ValidationRequestBody();
-    const createUser = new CreateUser(validationBody, userRepository)
+    const hashPassword = new HashProvider();
+    const createUser = new CreateUser(validationBody, userRepository, hashPassword);
     const createJWT = new CreateToken();
 
     return new UserController(createUser, createJWT);
