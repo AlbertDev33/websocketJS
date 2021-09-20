10791359269cec4eb5ba13321e093e133e5873b9
Albert <albert.rocha@r2t.company>
2021-09-20 20:50:40 -0300

    feature: fixed up validation logic

--- src/services/ValidationRequestBody/ValidationRequestBody.js
+++ src/services/ValidationRequestBody/ValidationRequestBody.js
@@ -2,15 +2,28 @@ import { ValidationBodyError } from "../../shared/errors/ValidationBodyError";
 
 const REQUEST_BODY_ERROR = 'Invalid request body.';
 
-const USER_SHAPE = {
-    name: '',
-    email: '',
-    password: '',
+const CREATE_USER_BODY = {
+    name: 1,
+    email: 2,
+    password: 3,
+};
+
+const LOGIN_BODY = {
+    email: 1,
+    password: 2,
 };
 
 export class ValidationRequestBody {
     validation(body) {
-        for (const key in USER_SHAPE) {
+        for (const key in CREATE_USER_BODY) {
+            if (!body.hasOwnProperty(key)) {
+                throw new ValidationBodyError(REQUEST_BODY_ERROR);
+            }
+        }
+    }
+
+    loginValidation(body) {
+        for (const key in LOGIN_BODY) {
             if (!body.hasOwnProperty(key)) {
                 throw new ValidationBodyError(REQUEST_BODY_ERROR);
             }
