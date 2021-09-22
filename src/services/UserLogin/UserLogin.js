26f005d17d75b4640d16fdccaf4e512548252d4e
Albert <albert.rocha@r2t.company>
2021-09-21 22:18:41 -0300

    feature: created

--- src/services/UserLogin/UserLogin.js
+++ src/services/UserLogin/UserLogin.js
@@ -1,19 +1,34 @@
 import { InvalidUserError } from "../../shared/errors/InvalidUserError";
 
-const LOGIN_ERROR_MESSAGE = 'User does not exits.';
+const IS_VALID_USER = 'User does not exits.';
+const LOGIN_ERROR_MESSAGE = 'E-mail or password not match.';
 
 export class UserLogin {
-    constructor(validationBody, userRepository) {
+    constructor(validationBody, userRepository, hashPassword) {
         this.validationBody = validationBody;
         this.userRepository = userRepository;
+        this.hashPassword = hashPassword;
     }
 
     async execute(body) {
         this.validationBody.loginValidation(body);
 
-        const user = await this.userRepository.findByEmail(body);
+        const { email, password } = body;
+
+        const user = await this.userRepository.findByEmail(email);
         if (!user) {
+            throw new InvalidUserError(IS_VALID_USER);
+        }
+
+        const isValidUser = await this.hashPassword.compareHash(password, user.password);
+
+        if (!isValidUser) {
             throw new InvalidUserError(LOGIN_ERROR_MESSAGE);
         }
+        
+        user.userId = user._id;
+        delete user._id;
+        delete user.password;
+        return user;
     }
 }
