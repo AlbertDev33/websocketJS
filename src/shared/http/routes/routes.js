e994a6f01fc8a4b2e265cd7ba7eb0d52f591613e
Albert <albert.rocha@r2t.company>
2021-09-21 22:17:59 -0300

    feature: created routes

--- src/shared/http/routes/routes.js
+++ src/shared/http/routes/routes.js
@@ -1,8 +1,19 @@
-import { Router } from 'express';
+import { response, Router } from 'express';
 import { makeCreateUser } from '../../../main/makeCreateUser';
+import { makeUserLogin } from '../../../main/makeUserLogin';
+import { makeVerifyUser } from '../../../main/makeVerifyUser';
 
 export const router = Router();
 
 router.post('/users', async (request, response) => {
     await makeCreateUser().handler(request, response);
+});
+
+router.post('/login', async (request, response) => {
+    await makeUserLogin().handler(request, response);
+});
+
+router.get('/verifyuser', async (request, response, next) => {
+    await makeVerifyUser().handler(request, response, next);
+    next();
 });
